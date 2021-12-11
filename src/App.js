5757f503593268d9a15cb68537bab1ce22987eac
Hakan Ünlü <unluhak@outlook.com>
2021-12-12 00:46:31 +0100

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import GlobalStyle from './components/GlobalStyle'
+import Nav from './components/Nav'
 import AboutUs from './pages/AboutUs'
+
 //Global Style
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   )
