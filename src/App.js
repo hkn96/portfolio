e8ac86ef9dec487df2925b1c4c9cbd68f66c155c
Hakan Ünlü <unluhak@outlook.com>
2021-12-15 00:41:49 +0100

    page details

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ContactUs from './pages/ContactUs'
 import OurWork from './pages/OurWork'
 //Router
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import MovieDetail from './pages/MovieDetail'
 
 //Global Style
 
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path='/' element={<AboutUs />}></Route>
         <Route path='/work' element={<OurWork />}></Route>
+        <Route path='/work/:id' element={<MovieDetail />}></Route>
         <Route path='/contact' element={<ContactUs />}></Route>
       </Routes>
     </div>
